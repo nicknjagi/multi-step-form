b1037d877e611c10f35b2eb7990bccf386bf4537
nicknjagi <njaginicholas@gmail.com>
2023-08-19 22:43:47 +0300

    add step tracker html

--- src/StepTracker.jsx
+++ src/StepTracker.jsx
@@ -2,8 +2,38 @@ import React from 'react'
 
 const StepTracker = () => {
   return (
-    <div className='step-tracker'>
-    </div>
+    <aside className='step-tracker'>
+      <div>
+        <div>
+          <button>1</button>
+          <div>
+            <span>STEP 1</span>
+            <h3>YOUR INFO</h3>
+          </div>
+        </div>
+        <div>
+          <button>2</button>
+          <div>
+            <span>STEP 2</span>
+            <h3>SELECT PLAN</h3>
+          </div>
+        </div>
+        <div>
+          <button>3</button>
+          <div>
+            <span>STEP 3</span>
+            <h3>ADD-ONS</h3>
+          </div>
+        </div>
+        <div>
+          <button>4</button>
+          <div>
+            <span>STEP 4</span>
+            <h3>SUMMARY</h3>
+          </div>
+        </div>
+      </div>
+    </aside>
   )
 }
 
