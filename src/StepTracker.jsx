ee74f654aac387ad5c253963fd87e3a09c87ad86
nicknjagi <njaginicholas@gmail.com>
2023-08-22 18:41:14 +0300

    add active class on step 1 button

--- src/StepTracker.jsx
+++ src/StepTracker.jsx
@@ -4,29 +4,29 @@ const StepTracker = () => {
   return (
     <aside className='step-tracker'>
         <div className='step'>
-          <button>1</button>
-          <div>
+          <button className='active'>1</button>
+          <div className='step-label-container'>
             <span>STEP 1</span>
             <h3>YOUR INFO</h3>
           </div>
         </div>
         <div className='step'>
           <button>2</button>
-          <div>
+          <div className='step-label-container'>
             <span>STEP 2</span>
             <h3>SELECT PLAN</h3>
           </div>
         </div>
         <div className='step'>
           <button>3</button>
-          <div>
+          <div className='step-label-container'>
             <span>STEP 3</span>
             <h3>ADD-ONS</h3>
           </div>
         </div>
         <div className='step'>
           <button>4</button>
-          <div>
+          <div className='step-label-container'>
             <span>STEP 4</span>
             <h3>SUMMARY</h3>
           </div>
