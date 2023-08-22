ddeffb97f0613d649c1e95e3aa4a2f55aae418b2
nicknjagi <njaginicholas@gmail.com>
2023-08-22 18:57:57 +0300

    add personal info component

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import { useState } from 'react'
 import StepTracker from './StepTracker'
+import PersonalInfo from './forms/PersonalInfo'
 
 function App() {
 
@@ -7,6 +8,7 @@ function App() {
     <>
       <main>
         <StepTracker />
+        <PersonalInfo />
       </main>
     </>
   )
