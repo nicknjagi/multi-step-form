ba67751a27c378d6bcc948cb73709df0fa575537
nicknjagi <njaginicholas@gmail.com>
2023-08-22 19:02:16 +0300

    add personal info component form jsx

--- src/forms/PersonalInfo.jsx
+++ src/forms/PersonalInfo.jsx
@@ -2,9 +2,16 @@ import React from 'react'
 
 const PersonalInfo = () => {
   return (
-    <div>
-      
-    </div>
+    <form>
+      <h2>Personal info</h2>
+      <p>Please provide your name, email address, and phone number.</p>
+      <label htmlFor="name">Name</label>
+      <input type="text" id="name" placeholder="e.g. Stephen King" />
+      <label htmlFor="email">Email Address</label>
+      <input type="email" name="email" id="email" placeholder='e.g stephenking@lorem.com' />
+      <label htmlFor="phone">Phone Number</label>
+      <input type="text" placeholder='e.g. +1 234 567 890' />
+    </form>
   )
 }
 
