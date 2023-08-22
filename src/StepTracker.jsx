a7437033eb2cca08c428b1724e8120c69cf0296e
nicknjagi <njaginicholas@gmail.com>
2023-08-22 18:31:50 +0300

    remove classless div wrapper

--- src/StepTracker.jsx
+++ src/StepTracker.jsx
@@ -3,36 +3,34 @@ import React from 'react'
 const StepTracker = () => {
   return (
     <aside className='step-tracker'>
-      <div>
-        <div>
+        <div className='step'>
           <button>1</button>
           <div>
             <span>STEP 1</span>
             <h3>YOUR INFO</h3>
           </div>
         </div>
-        <div>
+        <div className='step'>
           <button>2</button>
           <div>
             <span>STEP 2</span>
             <h3>SELECT PLAN</h3>
           </div>
         </div>
-        <div>
+        <div className='step'>
           <button>3</button>
           <div>
             <span>STEP 3</span>
             <h3>ADD-ONS</h3>
           </div>
         </div>
-        <div>
+        <div className='step'>
           <button>4</button>
           <div>
             <span>STEP 4</span>
             <h3>SUMMARY</h3>
           </div>
         </div>
-      </div>
     </aside>
   )
 }
