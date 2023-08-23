74325cca3422580a7d031a3ad3ae261e7430ca22
nicknjagi <njaginicholas@gmail.com>
2023-08-23 23:07:41 +0300

    remove button and replace with link

--- src/NextToggle.jsx
+++ src/NextToggle.jsx
@@ -3,11 +3,11 @@ import React from 'react'
 const NextToggle = ({handleNextStep, handlePrevStep, currentStep}) => {
   return (
     <div className="next-toggle">
-      <a href="#" onClick={handlePrevStep}>
+      <a href="#" className='prev' onClick={handlePrevStep}>
         Go Back
       </a>
       {currentStep !== 4 ? (
-        <button onClick={handleNextStep}>Next Step</button>
+        <a href="#" className='next-btn' onClick={handleNextStep}>Next Step</a>
       ) : (
         <input type="submit" value="Confirm" />
       )}
