237a77805ea1feba669c7a58ca989e8cbb2366e3
nicknjagi <njaginicholas@gmail.com>
2023-08-23 22:42:53 +0300

    create handle change function

--- src/forms/PersonalInfo.jsx
+++ src/forms/PersonalInfo.jsx
@@ -1,16 +1,21 @@
 import React from 'react'
 
 const PersonalInfo = () => {
+  
+  const handleChange = (e) => {
+    console.log(e.currentTarget.value);
+  }
+
   return (
     <div className='personal-info'>
       <h2>Personal info</h2>
       <p>Please provide your name, email address, and phone number.</p>
       <label htmlFor="name">Name</label>
-      <input type="text" id="name" placeholder="e.g. Stephen King" />
+      <input onChange={(e)=>handleChange(e)} type="text" id="name" placeholder="e.g. Stephen King" />
       <label htmlFor="email">Email Address</label>
-      <input type="email" name="email" id="email" placeholder='e.g stephenking@lorem.com' />
+      <input onChange={(e)=>handleChange(e)} type="email" name="email" id="email" placeholder='e.g stephenking@lorem.com' />
       <label htmlFor="phone">Phone Number</label>
-      <input type="number" placeholder='e.g. +1 234 567 890' />
+      <input onChange={(e)=>handleChange(e)} type="number" placeholder='e.g. +1 234 567 890' />
     </div>
   )
 }
