54733a2379d55dce19a4fbd62d886ae1bf6121d6
nicknjagi <njaginicholas@gmail.com>
2023-08-23 20:50:21 +0300

    create next step toggle component

---
+++ src/NextToggle.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const NextToggle = () => {
+  return (
+    <div className='next-toggle'>
+      <a href="#">Go Back</a>
+      <button>Next Step</button>
+    </div>
+  )
+}
+
+export default NextToggle
