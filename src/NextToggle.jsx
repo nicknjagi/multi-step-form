17f55b3afda52475206f89b2ddb200c6bde6681b
nicknjagi <njaginicholas@gmail.com>
2023-08-23 22:59:04 +0300

    add event listener on link and button

--- src/NextToggle.jsx
+++ src/NextToggle.jsx
@@ -1,10 +1,16 @@
 import React from 'react'
 
-const NextToggle = () => {
+const NextToggle = ({handleNextStep, handlePrevStep, currentStep}) => {
   return (
-    <div className='next-toggle'>
-      <a href="#">Go Back</a>
-      <button>Next Step</button>
+    <div className="next-toggle">
+      <a href="#" onClick={handlePrevStep}>
+        Go Back
+      </a>
+      {currentStep !== 4 ? (
+        <button onClick={handleNextStep}>Next Step</button>
+      ) : (
+        <input type="submit" value="Confirm" />
+      )}
     </div>
   )
 }
