2172c49e23d5671dc850d832e231fdb201aa26d3
nicknjagi <njaginicholas@gmail.com>
2023-08-24 18:53:31 +0300

    add active class on button that corresponds to the currentStep prop

--- src/StepTracker.jsx
+++ src/StepTracker.jsx
@@ -1,36 +1,36 @@
 import React from 'react'
 
-const StepTracker = () => {
+const StepTracker = ({currentStep}) => {
   return (
-    <aside className='step-tracker'>
-        <div className='step'>
-          <button className='active'>1</button>
-          <div className='step-label-container'>
-            <span>STEP 1</span>
-            <h3>YOUR INFO</h3>
-          </div>
+    <aside className="step-tracker">
+      <div className="step">
+        <button className={currentStep === 1 && 'active'}>1</button>
+        <div className="step-label-container">
+          <span>STEP 1</span>
+          <h3>YOUR INFO</h3>
         </div>
-        <div className='step'>
-          <button>2</button>
-          <div className='step-label-container'>
-            <span>STEP 2</span>
-            <h3>SELECT PLAN</h3>
-          </div>
+      </div>
+      <div className="step">
+        <button className={currentStep === 2 && 'active'}>2</button>
+        <div className="step-label-container">
+          <span>STEP 2</span>
+          <h3>SELECT PLAN</h3>
         </div>
-        <div className='step'>
-          <button>3</button>
-          <div className='step-label-container'>
-            <span>STEP 3</span>
-            <h3>ADD-ONS</h3>
-          </div>
+      </div>
+      <div className="step">
+        <button className={currentStep === 3 && 'active'}>3</button>
+        <div className="step-label-container">
+          <span>STEP 3</span>
+          <h3>ADD-ONS</h3>
         </div>
-        <div className='step'>
-          <button>4</button>
-          <div className='step-label-container'>
-            <span>STEP 4</span>
-            <h3>SUMMARY</h3>
-          </div>
+      </div>
+      <div className="step">
+        <button className={currentStep === 4 && 'active'}>4</button>
+        <div className="step-label-container">
+          <span>STEP 4</span>
+          <h3>SUMMARY</h3>
         </div>
+      </div>
     </aside>
   )
 }
