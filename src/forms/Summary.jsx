405747356a2c5cc9ebfcb24af3042976e6825b68
nicknjagi <njaginicholas@gmail.com>
2023-08-27 19:44:38 +0300

    add summary form jsx

--- src/forms/Summary.jsx
+++ src/forms/Summary.jsx
@@ -1,10 +1,33 @@
 import React from 'react'
 
-const Summary = () => {
+const Summary = ({setCurrentStep}) => {
   return (
-    <div>
-      
-    </div>
+    <section className='summary-container'>
+      <h2>Finishing up</h2>
+      <p>Double-check everything looks OK before confirming.</p>
+      <div className='summary'>
+        <div className="plan-summary">
+          <div>
+            <h3>Arcade(Monthly)</h3>
+            <button onClick={()=> setCurrentStep(2)} >Change</button>
+          </div>
+          <p>$9/mo</p>
+        </div>
+        <hr />
+        <div className="add-on-summary">
+          <p>Online service</p>
+          <p>+$1/mo</p>
+        </div>
+        <div className="add-on-summary">
+          <p>Online service</p>
+          <p>+$1/mo</p>
+        </div>
+      </div>
+        <div className="total-container">
+          <p>Total (per month)</p>
+          <p>+12/mo</p>
+        </div>
+    </section>
   )
 }
 
