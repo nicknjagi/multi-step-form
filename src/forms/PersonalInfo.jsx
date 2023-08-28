b4bba6e582bd12422d88f472d2803a245c4d2bb9
nicknjagi <njaginicholas@gmail.com>
2023-08-28 18:29:31 +0300

    destructure props

--- src/forms/PersonalInfo.jsx
+++ src/forms/PersonalInfo.jsx
@@ -1,21 +1,35 @@
 import React from 'react'
 
-const PersonalInfo = () => {
+const PersonalInfo = ({handleChange}) => {
   
-  const handleChange = (e) => {
-    console.log(e.currentTarget.value);
-  }
-
+  
   return (
-    <section className='personal-info'>
+    <section className="personal-info">
       <h2>Personal info</h2>
       <p>Please provide your name, email address, and phone number.</p>
       <label htmlFor="name">Name</label>
-      <input onChange={(e)=>handleChange(e)} type="text" id="name" placeholder="e.g. Stephen King" />
+      <input
+        onChange={(e) => handleChange(e)}
+        type="text"
+        id="name"
+        name="name"
+        placeholder="e.g. Stephen King"
+      />
       <label htmlFor="email">Email Address</label>
-      <input onChange={(e)=>handleChange(e)} type="email" name="email" id="email" placeholder='e.g stephenking@lorem.com' />
+      <input
+        onChange={(e) => handleChange(e)}
+        type="email"
+        name="email"
+        id="email"
+        placeholder="e.g stephenking@lorem.com"
+      />
       <label htmlFor="phone">Phone Number</label>
-      <input onChange={(e)=>handleChange(e)} type="number" placeholder='e.g. +1 234 567 890' />
+      <input
+        onChange={(e) => handleChange(e)}
+        type="number"
+        name="number"
+        placeholder="e.g. +1 234 567 890"
+      />
     </section>
   )
 }
