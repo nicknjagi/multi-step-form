21af490c3c6bd9556a97a6f9db706c5c9cd44055
nicknjagi <njaginicholas@gmail.com>
2023-08-30 18:43:21 +0300

    hide component on Submit

--- src/NextToggle.jsx
+++ src/NextToggle.jsx
@@ -1,17 +1,45 @@
 import React from 'react'
 
-const NextToggle = ({handleNextStep, handlePrevStep, currentStep}) => {
+const NextToggle = ({setCurrentStep, currentStep}) => {
+
+  const handleNextStep = () => {
+    if(currentStep === 5){
+      return
+    }
+    setCurrentStep((curr)=>curr + 1)
+  }
+
+  const handlePrevStep = () => {
+    if(currentStep === 1){
+      return
+    }
+    setCurrentStep((curr)=>curr - 1)
+  }
+  
   return (
-    <div className="next-toggle">
-      {currentStep !== 1 && <a href="#" className='prev' onClick={handlePrevStep}>
-        Go Back
-      </a>}
-      {currentStep !== 4 ? (
-        <a href="#" className='next-btn' onClick={handleNextStep}>Next Step</a>
-      ) : (
-        <input type="submit" className='confirm-btn' value="Confirm" />
-      )}
-    </div>
+    <>
+      {currentStep !== 5 ? (
+        <div className="next-toggle">
+          {currentStep !== 1 && (
+            <a href="#" className="prev" onClick={handlePrevStep}>
+              Go Back
+            </a>
+          )}
+          {currentStep !== 4 ? (
+            <a href="#" className="next-btn" onClick={handleNextStep}>
+              Next Step
+            </a>
+          ) : (
+            <input
+              onClick={handleNextStep}
+              type="submit"
+              className="confirm-btn"
+              value="Confirm"
+            />
+          )}
+        </div>
+      ) : null}
+    </>
   )
 }
 
