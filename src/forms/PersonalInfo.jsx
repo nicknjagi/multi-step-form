8c4d77e6c5e8d6df4032b9724febe023a945e1c7
nicknjagi <njaginicholas@gmail.com>
2023-09-06 20:44:30 +0300

    add classes to step 1 inputs

--- src/forms/PersonalInfo.jsx
+++ src/forms/PersonalInfo.jsx
@@ -10,11 +10,12 @@ const PersonalInfo = ({setUser,user,nameError, emailError,numberError}) => {
     <section className="personal-info">
       <h2>Personal info</h2>
       <p>Please provide your name, email address, and phone number.</p>
-      <label htmlFor="name">Name</label>
+      <label htmlFor="name"><span>Name</span><span className='error'>This field is required</span></label>
       <input
         onChange={handleChange}
         type="text"
         id="name"
+        className={nameError ? 'input-error' : 'input'}
         name="name"
         value={user.name}
         placeholder="e.g. Stephen King"
@@ -25,6 +26,7 @@ const PersonalInfo = ({setUser,user,nameError, emailError,numberError}) => {
         type="email"
         name="email"
         id="email"
+        className={emailError ? 'input-error' : 'input'}
         value={user.email}
         placeholder="e.g stephenking@lorem.com"
       />
@@ -34,6 +36,7 @@ const PersonalInfo = ({setUser,user,nameError, emailError,numberError}) => {
         type="number"
         name="number"
         id='number'
+        className={numberError ? 'input-error' : 'input'}
         value={user.number}
         placeholder="e.g. +1 234 567 890"
       />
